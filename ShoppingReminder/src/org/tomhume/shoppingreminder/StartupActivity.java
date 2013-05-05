bf4e14847807e99f403cf8ca4409dd897d107c54
Tom Hume <twhume@gmail.com>
2013-05-05 18:06:32 +0100

    Writing into the spreadsheet, LIKE A BOSS

--- ShoppingReminder/src/org/tomhume/shoppingreminder/StartupActivity.java
+++ ShoppingReminder/src/org/tomhume/shoppingreminder/StartupActivity.java
@@ -1,12 +1,18 @@
 package org.tomhume.shoppingreminder;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.data.TextConstruct;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.spreadsheet.ListFeed;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
+import com.google.gdata.data.spreadsheet.Worksheet;
+import com.google.gdata.data.spreadsheet.WorksheetEntry;
 import com.google.gdata.util.ServiceException;
 
 import android.os.AsyncTask;
@@ -54,12 +60,7 @@ public class StartupActivity extends Activity {
 		
 	}
 	
-	private Account getGMailAccount(Account[] accounts) {
-		for (Account a: accounts) {
-			if (a.type.equals("com.google") && a.name.endsWith("gmail.com")) return a;
-		}
-		return null;
-	}
+
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 	    if (requestCode == 0) {
@@ -81,6 +82,23 @@ public class StartupActivity extends Activity {
 		return true;
 	}
 
+	private Account getGMailAccount(Account[] accounts) {
+		for (Account a: accounts) {
+			if (a.type.equals("com.google") && a.name.endsWith("gmail.com")) return a;
+		}
+		return null;
+	}
+	
+	private SpreadsheetEntry getShoppingList(SpreadsheetService ss) throws IOException, ServiceException {
+	    URL metafeedUrl = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+	    SpreadsheetFeed feed = ss.getFeed(metafeedUrl, SpreadsheetFeed.class);
+
+	    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+	    for (SpreadsheetEntry sheet: spreadsheets) {
+	    	if (sheet.getTitle().getPlainText().equalsIgnoreCase("shopping list")) return sheet;
+	    }
+	    return null;
+	}
 	
 	private class AddSpreadsheetTask extends AsyncTask<Activity, Integer, Long> {
 	     protected Long doInBackground(Activity... acts) {
@@ -90,23 +108,28 @@ public class StartupActivity extends Activity {
 	        	if (gmail!=null) {
 	        		AccountManagerFuture<Bundle> amf = amgr.getAuthToken(gmail, "wise", null, acts[0], null, null);
 					try {
+						
 						Bundle authTokenBundle = amf.getResult();
 						String authToken = authTokenBundle.getString(AccountManager.KEY_AUTHTOKEN);
-		        		Log.d(TAG, "authToken="+authToken);
-		        		
-		        		
 		        		SpreadsheetService ss = new SpreadsheetService("Spreadsheet");
 		        	    ss.setProtocolVersion(SpreadsheetService.Versions.V3);
 		        	    ss.setHeader("Authorization", "GoogleLogin auth=" + authToken);
-		        	    		        	    
-		        	    
-		        	    URL metafeedUrl = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
-		        	    SpreadsheetFeed feed = ss.getFeed(metafeedUrl, SpreadsheetFeed.class);
 
-		        	    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-		        	    for (SpreadsheetEntry sheet: spreadsheets) {
-		        	    	Log.d(TAG, "ssheet="+sheet.getTitle().getPlainText());
-		        	    }
+		        	    SpreadsheetEntry theSheet = getShoppingList(ss);
+		        	    WorksheetEntry ws = theSheet.getWorksheets().get(0);
+		        	    
+
+		        	    
+		        	    URL listFeedUrl = ws.getListFeedUrl();
+		        	    ListFeed listFeed = ss.getFeed(listFeedUrl, ListFeed.class);
+
+		        	    // Create a local representation of the new row.
+		        	    ListEntry row = new ListEntry();
+		        	    row.getCustomElements().setValueLocal("item", "Joe");
+		        	    // Send the new row to the API for insertion.
+		        	    row = ss.insert(listFeedUrl, row);
+		        	    
+		        	    Log.d(TAG, "wrote row");
 		        		
 					} catch (OperationCanceledException e) {
 						e.printStackTrace();
